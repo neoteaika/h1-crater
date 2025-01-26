2f6f203ede54d11a9af621f1096d3a1d11d04913
Neo Te Aika <neo.teaika@gmail.com>
2025-01-27 02:13:29 +1300

    updated

--- __crater FULLTEST.bat
+++ __crater FULLTEST.bat
@@ -1,3 +1,3 @@
 hell_tool.exe structure levels\neo\crater crater
-hell_tool.exe lightmaps levels\neo\crater\crater all 1 0.000001
+hell_tool.exe lightmaps levels\neo\crater\crater all 1 0.00000001
 hell_halo_tag_test -window -vidmode 1280,960,180 -nosound -nonetwork -nojoystick -nogamma -exec _crater.txt
